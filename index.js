a369d5c18d55765a7fe5bb41932798f0f99138f5
lacoperon <elliot.james.williams@gmail.com>
2017-06-27 23:53:33 -0500

    Tried fixing post of FB Messenger Bot

--- index.js
+++ index.js
@@ -62,6 +62,39 @@ app.get('/webhook', function(req, res) {
   }
 });
 
+app.post('/', function (req, res) {
+	var data = req.body;
+
+  // Make sure this is a page subscription
+  if (data.object == 'page') {
+    // Iterate over each entry
+    // There may be multiple if batched
+    data.entry.forEach(function(pageEntry) {
+      var pageID = pageEntry.id;
+      var timeOfEvent = pageEntry.time;
+
+      // Iterate over each messaging event
+      pageEntry.messaging.forEach(function(messagingEvent) {
+        if (messagingEvent.optin) {
+          receivedAuthentication(messagingEvent);
+        } else if (messagingEvent.message) {
+          receivedMessage(messagingEvent);
+        } else if (messagingEvent.delivery) {
+          receivedDeliveryConfirmation(messagingEvent);
+        } else if (messagingEvent.postback) {
+          receivedPostback(messagingEvent);
+        } else if (messagingEvent.read) {
+          receivedMessageRead(messagingEvent);
+        } else if (messagingEvent.account_linking) {
+          receivedAccountLink(messagingEvent);
+        } else {
+          console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+        }
+      });
+    });
+	);
+
+
 
 /*
  * All callbacks for Messenger are POST-ed. They will be sent to the same
