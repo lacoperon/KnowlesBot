517a117279f1a0f7017753692acc1cd65ae11857
lacoperon <elliot.james.williams@gmail.com>
2017-06-27 22:18:21 -0500

    Added hub challenge validation for the bot

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
-	res.send('Hello world, I am a chat bot')
+	res.status(200).send(req.query['hub.challenge']);
 })
 
 // for Facebook verification
