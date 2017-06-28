29a184998f349714f73e7659b24da4cc34860013
lacoperon <elliot.james.williams@gmail.com>
2017-06-28 00:53:43 -0500

    Ah, I forgot to send a 200 status. Makes sense why it wasn't working

--- index.js
+++ index.js
@@ -62,49 +62,8 @@ app.get('/webhook', function(req, res) {
   }
 });
 
-// app.post('/', function (req, res) {
-// 	var data = req.body;
-//
-//   // Make sure this is a page subscription
-//   if (data.object == 'page') {
-//     // Iterate over each entry
-//     // There may be multiple if batched
-//     data.entry.forEach(function(pageEntry) {
-//       var pageID = pageEntry.id;
-//       var timeOfEvent = pageEntry.time;
-//
-//       // Iterate over each messaging event
-//       pageEntry.messaging.forEach(function(messagingEvent) {
-//         if (messagingEvent.optin) {
-//           receivedAuthentication(messagingEvent);
-//         } else if (messagingEvent.message) {
-//           receivedMessage(messagingEvent);
-//         } else if (messagingEvent.delivery) {
-//           receivedDeliveryConfirmation(messagingEvent);
-//         } else if (messagingEvent.postback) {
-//           receivedPostback(messagingEvent);
-//         } else if (messagingEvent.read) {
-//           receivedMessageRead(messagingEvent);
-//         } else if (messagingEvent.account_linking) {
-//           receivedAccountLink(messagingEvent);
-//         } else {
-//           console.log("Webhook received unknown messagingEvent: ", messagingEvent);
-//         }
-//       });
-//     })};
-// 	});
-
-
-
-/*
- * All callbacks for Messenger are POST-ed. They will be sent to the same
- * webhook. Be sure to subscribe your app to your page to receive callbacks
- * for your page.
- * https://developers.facebook.com/docs/messenger-platform/product-overview/setup#subscribe_app
- *
- */
-app.post('/webhook', function (req, res) {
-  var data = req.body;
+app.post('/', function (req, res) {
+	var data = req.body;
 
   // Make sure this is a page subscription
   if (data.object == 'page') {
@@ -131,16 +90,64 @@ app.post('/webhook', function (req, res) {
         } else {
           console.log("Webhook received unknown messagingEvent: ", messagingEvent);
         }
+				res.sendStatus(200);
       });
-    });
+    })};
+	});
 
-    // Assume all went well.
-    //
-    // You must send back a 200, within 20 seconds, to let us know you've
-    // successfully received the callback. Otherwise, the request will time out.
-    res.sendStatus(200);
-  }
-});
+
+
+/*
+ * All callbacks for Messenger are POST-ed. They will be sent to the same
+ * webhook. Be sure to subscribe your app to your page to receive callbacks
+ * for your page.
+ * https://developers.facebook.com/docs/messenger-platform/product-overview/setup#subscribe_app
+ *
+ */
+// app.post('/webhook', function (req, res) {
+//   var data = req.body;
+//
+//   // Make sure this is a page subscription
+//   if (data.object == 'page') {
+//     // Iterate over each entry
+//     // There may be multiple if batched
+//     data.entry.forEach(function(pageEntry) {
+//       var pageID = pageEntry.id;
+//       var timeOfEvent = pageEntry.time;
+//
+//       // Iterate over each messaging event
+//       pageEntry.messaging.forEach(function(messagingEvent) {
+//         if (messagingEvent.optin) {
+//           receivedAuthentication(messagingEvent);
+// 					res.sendStatus(200);
+//         } else if (messagingEvent.message) {
+//           receivedMessage(messagingEvent);
+// 					res.sendStatus(200);
+//         } else if (messagingEvent.delivery) {
+//           receivedDeliveryConfirmation(messagingEvent);
+// 					res.sendStatus(200);
+//         } else if (messagingEvent.postback) {
+//           receivedPostback(messagingEvent);
+// 					res.sendStatus(200);
+//         } else if (messagingEvent.read) {
+//           receivedMessageRead(messagingEvent);
+// 					res.sendStatus(200);
+//         } else if (messagingEvent.account_linking) {
+//           receivedAccountLink(messagingEvent);
+// 					res.sendStatus(200);
+//         } else {
+//           console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+//         }
+//       });
+//     });
+//
+//     // Assume all went well.
+//     //
+//     // You must send back a 200, within 20 seconds, to let us know you've
+//     // successfully received the callback. Otherwise, the request will time out.
+//     res.sendStatus(200);
+//   }
+// });
 
 /*
  * This path is used for account linking. The account linking call-to-action
