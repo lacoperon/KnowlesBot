ab3e4b395554fcc839a0b3ecbde53de94a118842
lacoperon <elliot.james.williams@gmail.com>
2017-06-29 19:29:41 -0500

    Trying to containerize the FB Messenger Code

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ const
   crypto = require('crypto'),
   express = require('express'),
   https = require('https'),
-  request = require('request');
+  request = require('request'),
+  printNumber = require('./printNumber');
+
+console.log(printNumber.THREE);
 
 var app = express();
 app.set('port', process.env.PORT || 5000);
@@ -31,15 +34,10 @@ app.use(express.static('public'));
 
 // App Secret can be retrieved from the App Dashboard
 const APP_SECRET = (process.env.APP_SECRET)
-
-// Arbitrary value used to validate a webhook
 const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN)
-
-// Generate a page access token for your page from the App Dashboard
 const PAGE_ACCESS_TOKEN = (process.env.FB_PAGE_ACCESS_TOKEN)
-// URL where the app is running (include protocol). Used to point to scripts and
-// assets located at this address.
 const SERVER_URL = (process.env.SERVER_URL)
+
 
 if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN && SERVER_URL)) {
   console.error("Missing config values");
---
+++ printNumber.js
@@ -0,0 +1,40 @@
+class fb {
+  static init() {
+
+  }
+
+  /*
+   * Verify that the callback came from Facebook. Using the App Secret from
+   * the App Dashboard, we can verify the signature that is sent with each
+   * callback in the x-hub-signature field, located in the header.
+   *
+   * https://developers.facebook.com/docs/graph-api/webhooks#setup
+   *
+   */
+
+  static verifyRequestSignature(req, res, buf) {
+    var signature = req.headers["x-hub-signature"];
+
+    if (!signature) {
+      // For testing, let's log an error. In production, you should throw an
+      // error.
+      console.error("Couldn't validate the signature.");
+    } else {
+      var elements = signature.split('=');
+      var method = elements[0];
+      var signatureHash = elements[1];
+
+      var expectedHash = crypto.createHmac('sha1', APP_SECRET)
+                          .update(buf)
+                          .digest('hex');
+
+      if (signatureHash != expectedHash) {
+        throw new Error("Couldn't validate the request signature.");
+      }
+    }
+  }
+}
+
+fb.THREE = 3;
+
+module.exports = fb;
