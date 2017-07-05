7259100b75caab70ac262fd8b46965fd790d33c2
lacoperon <elliot.james.williams@gmail.com>
2017-07-05 14:24:39 -0500

    Refactored code into three files. messenger.ts for the Messenger API-related functions messenger_types.ts for all necessary Interfaces for Messenger API-related activities index.ts for the code necessary to run the server using express

---
+++ src/messenger_types.ts
@@ -0,0 +1,50 @@
+
+/*
+This is the 'Messenger Types' file, in which I explicitly declare interfaces
+used by Facebook's Messenger API, based upon my own research into their docs.
+
+-@lacoperon
+*/
+export interface Sender {
+  id : string;
+}
+
+export interface Recipient {
+  id : string;
+}
+
+export interface Event {
+  sender : Sender;
+  recipient : Recipient;
+  timestamp : number;
+  message : Message;
+  postback : Postback;
+}
+
+export interface Message {
+  is_echo : boolean;
+  mid : string; // message ID
+  app_id : string;
+  metadata : string;
+  text : string;
+  attachments : any;
+  quick_reply : QuickReply;
+}
+
+export interface QuickReply {
+  content_type : "location" | "text";
+  title? : string;
+  payload? : string;
+  image_url? : string;
+}
+
+export interface Referral {
+  ref : any;
+  source : "SHORTLINK" | "ADS";
+  type: string;
+}
+
+export interface Postback {
+  payload : string;
+  referral : Referral;
+}
